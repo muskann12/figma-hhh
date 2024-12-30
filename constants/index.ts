1ee78cc221f55ceff0d0489353a0dc6ecaadf849
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2024-12-30 05:10:19 +0500

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,7 @@
 export const Navlinks = [
   { name: 'Home', link: '/' },
-  { name: 'Shop', link: '/casual' },  // Change to /casual page
-  { name: 'About', link: '/about' },
-  { name: 'Contact', link: '/contact' },
+  { name: 'Shop', link: '/casual' },  
+  { name: 'On sale', link: '/' },
+  { name: 'New arrivals', link: '/' },
+  { name: 'Brands', link: '/' },
 ];
